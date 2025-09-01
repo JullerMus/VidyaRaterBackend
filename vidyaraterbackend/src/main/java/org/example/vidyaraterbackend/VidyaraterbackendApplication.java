f99c7c4001db58226674c2aae0ab3f444bbeb658
JullerMus <juliusclykkegaard@gmail.com>
2025-09-01 08:59:37 +0200

    Created Spring web project

---
+++ vidyaraterbackend/src/main/java/org/example/vidyaraterbackend/VidyaraterbackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.vidyaraterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidyaraterbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VidyaraterbackendApplication.class, args);
+    }
+
+}
---
+++ vidyaraterbackend/src/test/java/org/example/vidyaraterbackend/VidyaraterbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.vidyaraterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidyaraterbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
